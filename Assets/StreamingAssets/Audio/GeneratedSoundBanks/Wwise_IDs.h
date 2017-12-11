f0b3b8938dde78609503b1f9250f293aa43966b8
Harry Boam <harry@pitstopproductions.co.uk>
2017-12-11 17:17:20 +0000

    Added new sounds and events to Wwise

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -15,7 +15,53 @@ namespace AK
     {
         static const AkUniqueID PENGUINQUACK = 3719585952U;
         static const AkUniqueID PENGUINSQUEAK = 2594679953U;
+        static const AkUniqueID PENGUINSWIM = 4055301235U;
+        static const AkUniqueID POLARAMBIENCE = 4107323607U;
+        static const AkUniqueID POLARMUSIC = 3060268180U;
+        static const AkUniqueID WATERSPLASH = 777507535U;
+        static const AkUniqueID WHALESPRAY = 893632031U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace PENGUINQUACK
+        {
+            static const AkUniqueID GROUP = 3719585952U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID QUACKIDLE = 1696485716U;
+                static const AkUniqueID QUACKPICKEDUP = 1393799693U;
+            } // namespace SWITCH
+        } // namespace PENGUINQUACK
+
+        namespace PENGUINSWIM
+        {
+            static const AkUniqueID GROUP = 4055301235U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FAST = 2965380179U;
+                static const AkUniqueID FASTUNDERWATER = 2059843340U;
+                static const AkUniqueID SLOW = 787604482U;
+                static const AkUniqueID SLOWUNDERWATER = 3949372325U;
+            } // namespace SWITCH
+        } // namespace PENGUINSWIM
+
+        namespace WATERSPLASH
+        {
+            static const AkUniqueID GROUP = 777507535U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LARGE = 4284352190U;
+                static const AkUniqueID MEDIUM = 2849147824U;
+                static const AkUniqueID SMALL = 1846755610U;
+                static const AkUniqueID SMALLSOFT = 1108843758U;
+            } // namespace SWITCH
+        } // namespace WATERSPLASH
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
