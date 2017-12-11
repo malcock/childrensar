b1d920904096cdd3ff1ef2024eec638807f3b2fe
Unknown <martinalcock84@gmail.com>
2017-12-11 21:19:23 +0000

    Reinstalled WWise, added particle controller
    birds poop, whale fountains

---
+++ Assets/Scripts/Splashable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splashable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Water")
+        {
+            GameObject splash = Instantiate(Resources.Load("Splash", typeof(GameObject)), transform.position, Quaternion.identity) as GameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Water")
+        {
+            GameObject splash = Instantiate(Resources.Load("Splash", typeof(GameObject)), transform.position, Quaternion.identity) as GameObject;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/WhalePath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhalePath : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RandomRotation(){
+        transform.parent.rotation = Quaternion.Euler(0, Random.value * 360, 0);
+    }
+}
