619908c82c59b0482cbda9be4c210250ac53bffc
Kei <KeiTagura@Hotmail.co.jp>
2017-12-20 17:43:54 +0900

    ThrowFlick
    Added throwing controller.
    The sampel sceen I created is inside teh folder -Kei.

---
+++ Assets/-Kei/Code/Ctrl_Follow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl_Follow : MonoBehaviour {
+
+    // Use this for initializ
+    public GameObject fish;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fish.transform.position = transform.position;
+	}
+}
